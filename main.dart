1963c4203b334f1f009fe4f539d46261a7518495
yan <31926275+JeanRodriguez1@users.noreply.github.com>
2025-02-27 12:17:13 -0600

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,3 +1,27 @@
 void main() {
+  // this is a comment
   print('Hello, World!');
 }
+
+/*
+  This is a multi-line comment
+  It can span multiple lines
+  It is often used to describe what a function does
+  or to comment out large blocks of code
+*/
+
+// Prymitive types
+
+// int - 1, 2, 3, -5
+// double - 01, 2.5, 3.14, -0.2
+// bool - true, false
+// String - 'Hello, World!', "Hello, World!"
+
+int ageOfPet =
+    25; // nombre de variable no puede iniciar con numeros o caracteres especiales.
+double pi = 10.5;
+bool isAlive = true;
+String name = 'John Doe';
+// Dynamic types
+
+// Instance Types
